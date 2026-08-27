573647c7888ca54b797f54e8a4443529ae2710b9
Shubham Bansal <sm325642@gmail.com>
2026-08-27 23:28:35 +0530

    final

--- Dispatch/frontend/lib/axios.js
+++ Dispatch/frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5555/api",
+    baseURL: `${import.meta.env.VITEAPI_URL}/api`,
     withCredentials: true,
     timeout: 10000,
     headers: {
