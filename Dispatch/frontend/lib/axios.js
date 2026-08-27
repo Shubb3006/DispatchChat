2c4884b2697dc83495da470db522313dbe1799aa
Shubham Bansal <sm325642@gmail.com>
2026-08-27 23:43:18 +0530

    final

--- Dispatch/frontend/lib/axios.js
+++ Dispatch/frontend/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITEAPI_URL}/api`,
-    withCredentials: true,
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json"
-    }
-}); 
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
